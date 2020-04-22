2b19769d1b83b537947a90b408e809f129f0a746
adriano.digiere <adriano.digiere@abc71.com.br>
2020-04-22 09:21:54 -0300

    Início restrições de movimentações

--- xadrez-console/xadrez-console/tabuleiro/Peca.cs
+++ xadrez-console/xadrez-console/tabuleiro/Peca.cs
@@ -19,11 +19,27 @@ namespace tabuleiro
             qtdeMovimentos = 0;
         }
 
-        public abstract bool[,] movimentosPossiveis();
+        public bool existeMovimentosPossiveis()
+        {
+            bool[,] mat = movimentosPossiveis();
+
+            for (int i = 0; i < tab.linhas; i++)
+                for (int j = 0; j < tab.colunas; j++)
+                    if (mat[i, j])
+                        return (true);
+
+            return (false);
+        }
+
+        public bool podeMoverPara(Posicao pos)
+        {
+            return movimentosPossiveis()[pos.Linha, pos.Coluna];
+        }
 
         public void incrementarQtdeMovimentos()
         {
             qtdeMovimentos++;
         }
+        public abstract bool[,] movimentosPossiveis();
     }
 }
